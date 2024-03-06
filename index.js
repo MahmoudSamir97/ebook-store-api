0363e0964937c67104f009d8633c31d29b690079
Your Name <you@example.com>
2024-03-06 23:52:13 +0200

    Menna

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const userRouter = require('./src/routes/userRoutes');
 const categoryRouter = require('./src/routes/category.routes');
-
+const router = require('./src/routes/bookRoutes.js');
+app.use('/', router);
 app.use(express.json());
 app.use('/user', userRouter);
 app.use('/category', categoryRouter);
