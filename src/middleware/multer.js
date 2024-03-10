7072703bf5a3dc0f877949b6b92a5e2e9dd31d95
Your Name <you@example.com>
2024-03-11 01:56:30 +0200

    edite

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,15 +1,3 @@
-// const multer = require('multer');
-
-// const storage = multer.diskStorage({
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname);
-//     },
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
-
 const multer = require('multer');
 const maxSize = 4 * 1024 * 1024;
 const fileStorageEngine = multer.memoryStorage();
